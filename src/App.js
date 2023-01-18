c8497f7813c649ecae0529fbcb6d98dcf3064b6a
Akshatha Hebbar <akshathahebbar1234@gmail.com>
2023-01-17 22:42:12 -0800

    Added routes,navigation and base styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from "./components/layout/Navbar";
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
+import Fotter from "./components/layout/footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+/*
+* If we try to use links without the router than we get an error
+* */
 function App() {
   return (
-    <div className='bg-purple-500'>
-        <h1 className='text-xl'> Hello World</h1>
-        <button className='btn'>Click</button>
-    </div>
+    <Router>
+        <div className="flex flex-col justify-between h-screen">
+            <Navbar/>
+            <main className='container mx-auto px-3 pb-12'>
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/About' element={<About/>}/>
+                    <Route path='/notfound' element={<NotFound/>}/>
+                    <Route path='/*' element={<NotFound/>}/>
+                </Routes>
+            </main>
+            <Fotter/>
+        </div>
+    </Router>
   );
 }
 
